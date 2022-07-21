ab76f44092df0179c4f7449d0e16db454f6fdcbb
Nguyen Khac Hieu <hieu.h.nguyen@aalto.fi>
2022-07-21 12:27:29 +0700

    add build static

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
+app.use(express.static('build'));
 let notes = [
     { 
       "id": 1,
